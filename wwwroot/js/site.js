796ed6b9250bca54b5223484135f01144104eb10
igutterman <50182831+igutterman@users.noreply.github.com>
2022-08-23 18:56:54 +0200

    Add files via upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,4 +3,43 @@
 
 // Write your JavaScript code.
 
-    
+
+<script>
+
+var fullPathsMap = new Map();
+var thumbPathsMap = new Map();
+
+
+    @foreach (ForumThread thread in Model.Threads) {
+        @foreach (Post post in thread) {
+            if (post.HasImage) {
+
+                <text>
+                    fullPathsMap.set('@post.Files[0].FullFileName', '@post.Files[0].FullFileName')
+                    thumbPathsMap.set('@post.Files[0].FullFileName', '@post.Files[0].ThumbFileName')
+                </text>
+
+            }
+        }
+    }
+
+</script>
+
+<script>
+
+    function resizeImage(event) { 
+
+        var image = event.target;
+        
+
+        if (image.getAttribute("data-expanded") === "0") {
+            image.src = '/images/' + fullPathsMap.get(image.id);
+            image.dataset.expanded = "1";
+        } else {
+            image.src = '/images/' + thumbPathsMap.get(image.id);
+            image.dataset.expanded = "0";
+        }
+
+
+    }
+</script>
